e5d82789c3772b021956ddd1c2cda079e85d977c
phucgiang <phuc.giang.ry@renesas.com>
2023-08-19 15:24:23 +0700

    update router

--- src/App.js
+++ src/App.js
@@ -3,19 +3,18 @@ import './App.css';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
 // import AuthDetail from './components/auth/AuthDetail';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path = "/">
-        <Route index element={<SignIn/>}></Route>
-        <Route path = "login" element= {<SignIn/>}></Route>
-        <Route path = "register" element ={<SignUp/>}></Route>
-      </Route>
+    <HashRouter>
+    <Routes>      
+      <Route path = "/" element={<SignIn/>}></Route>
+      <Route path = "login" element= {<SignIn/>}></Route>
+      <Route path = "register" element ={<SignUp/>}></Route>
+
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
