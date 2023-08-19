fb643d3b9b437e508a976410ac960ae49c8a45a8
phucgiang <phuc.giang.ry@renesas.com>
2023-08-19 14:45:47 +0700

    add sign out method

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
+import AuthDetail from './components/auth/AuthDetail';
 
 function App() {
   return (
     <div className="App">
       <SignIn/>
       <SignUp/>
+      <AuthDetail/>
     </div>
   );
 }
